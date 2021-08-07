b3f2693febbff79e992aa1d348eda05d58e399d4
Gurnehmat K. Dhindsa <gurnehmat.dhindsa@gmail.com>
2021-08-07 20:28:10 +0530

    await, fetch

---
+++ svelte-project/src/User.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let name;
+    export let username;
+    export let email;
+    export let address;
+
+</script>
+<main>
+    <div>
+        <h2>{name}</h2>
+        <p>{username}</p>
+        <p>{email}</p>
+        <p>{address.city}</p>
+    </div>
+</main>
